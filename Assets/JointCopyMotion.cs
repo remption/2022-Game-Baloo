856338267d9af45b2375d31e48c087ab3fabeef2
remption <kelseym2731@gmail.com>
2022-03-03 14:15:19 -0600

    scripting Updates
    -loop several anims in test controller
    -the joint rotate extension script added
    - using said extensions script

--- Assets/JointCopyMotion.cs
+++ Assets/JointCopyMotion.cs
@@ -6,19 +6,25 @@ using UnityEngine;
 public class JointCopyMotion : MonoBehaviour
 {
     public Transform toCopy;
+    public bool local = true;
     protected ConfigurableJoint myJoint;
 
-    private Vector3 targetAnchorStartPos;
-    private Quaternion targetAnchorStartRot;
-
+    private Vector3 toCopyStartPos;
+    private Quaternion toCopyStartRot;
     // Start is called before the first frame update
     void Start()
     {
         myJoint = GetComponent<ConfigurableJoint>();
         if (toCopy)
         {
-            targetAnchorStartPos = toCopy.position;//todo - better to do this in local space and stuff?
-            targetAnchorStartRot = toCopy.rotation;
+            toCopyStartPos = toCopy.position;//todo - better to do this in local space and stuff?
+            if (local) {
+                toCopyStartRot = toCopy.localRotation;
+                myJoint.configuredInWorldSpace = false;
+            } else {
+                toCopyStartRot = toCopy.rotation;
+                myJoint.configuredInWorldSpace = true;
+            }
         }
     }
 
@@ -27,20 +33,38 @@ public class JointCopyMotion : MonoBehaviour
     {
         if (toCopy && myJoint)
         {
-            CalculateOffsetAndStuff();
+            // CalculateOffsetAndStuff();
+            //   CalculateOffsetAndStuffv2();
+            if (local) ConfigurableJointExtensions.SetTargetRotationLocal(myJoint, toCopy.localRotation, toCopyStartRot);
+           else  ConfigurableJointExtensions.SetTargetRotation(myJoint, toCopy.rotation, toCopyStartRot);
         }
     }
 
     void CalculateOffsetAndStuff()
     {
-        Vector3 targetPos = targetAnchorStartPos - toCopy.position; //current offset in position from where it started at the v beginning
-        Quaternion targetRot = Quaternion.Inverse( toCopy.rotation) * targetAnchorStartRot; //same,but with rotation
+        Vector3 targetPos = toCopyStartPos - toCopy.position; //current offset in position from where it started at the v beginning
+        Quaternion targetRot = Quaternion.Inverse( toCopy.rotation) * toCopyStartRot; //same,but with rotation
 
         myJoint.targetRotation = targetRot.normalized;
         myJoint.targetPosition = targetPos;
+
+       // Quaternion nooo = new Quaternion();
+        //nooo.eulerAngles = Vector3.zero;
     }
 
     void CalculateOffsetAndStuffv2() {
-        
+    }
+
+    Vector3 clampRotTo180s(Vector3 toClamp) {
+        while (toClamp.x < -180) toClamp.x += 180;
+        while (toClamp.x > 180) toClamp.x -= 180;
+
+        while (toClamp.y < -180) toClamp.y += 180;
+        while (toClamp.y > 180) toClamp.y -= 180;
+
+        while (toClamp.z < -180) toClamp.z += 180;
+        while (toClamp.z > 180) toClamp.z -= 180;
+
+        return toClamp;
     }
 }
