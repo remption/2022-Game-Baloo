571011604d18dc931da7d5a77bd3b7f2a1a39f38
remption <kelseym2731@gmail.com>
2022-03-16 19:39:33 -0500

    Config Joint Editor Almost Perfect~
    Just need the foldouts to maintain state, and the quaternion editor bit :)

--- Assets/RagdollExperimetns/ConfigurableJointWork/Editor/ConfigurableJointInspector.cs
+++ Assets/RagdollExperimetns/ConfigurableJointWork/Editor/ConfigurableJointInspector.cs
@@ -11,45 +11,26 @@ public class ConfigurableJointInspector : Editor
     public VisualTreeAsset m_InspectorXML;
     public override VisualElement CreateInspectorGUI()
     {
-        
-  
         VisualElement myInspector = new VisualElement();
         m_InspectorXML.CloneTree(myInspector);
 
-        ConfigurableJoint j;
-       // j.targetRotation.eulerAngles
-       // Rigidbody body = new Rigidbody();
-
-        //WORKS
-         IMGUIContainer defaultInspect = myInspector.Q<IMGUIContainer>("DefaultContainer");
+        //WORKS - Uses the IMGUIContainer I added in the builder to draw the default inspector.
+        //This lets me grab property bindings easily as I work!
+         /*IMGUIContainer defaultInspect = myInspector.Q<IMGUIContainer>("DefaultContainer");
         if (defaultInspect.enabledInHierarchy)
-        defaultInspect.onGUIHandler = () => DrawDefaultInspector();
+            defaultInspect.onGUIHandler = () => DrawDefaultInspector();*/
 
-        //Doesn't work but should according to docs!
+        //Doesn't work but should according to docs?
         // VisualElement defaultFoldout = myInspector.Q("Default_Inspector");
         //InspectorElement.FillDefaultInspector(defaultFoldout, serializedObject, this);
 
-
-        //test.style.display = DisplayStyle.None;
-        // return base.CreateInspectorGUI();
-
-
         return myInspector;
-    }
+    }/**/
 
-
-   /* public override void OnInspectorGUI()
+    public void HotPantsRomance()
     {
-        DrawPropertiesExcluding(serializedObject, );
-        Rigidbody buts;
 
-       SerializedProperty curProp = serializedObject.GetIterator();
-        bool stillGoing = curProp != null;
-       while(stillGoing)
-        {
-            EditorGUILayout.PropertyField(curProp);
-            //TODO - take care of the children recurisively
-            stillGoing = curProp.Next(false);
-        }
-    }*/
+
+        //return true;
+    }
 }
