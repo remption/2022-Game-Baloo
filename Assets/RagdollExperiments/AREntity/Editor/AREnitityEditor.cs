102e4194d36368f668c00d3e4d8375adb1e1ca53
remption <kelseym2731@gmail.com>
2022-04-24 21:30:42 -0500

    Physics clone pretty solid

--- Assets/RagdollExperiments/AREntity/Editor/AREnitityEditor.cs
+++ Assets/RagdollExperiments/AREntity/Editor/AREnitityEditor.cs
@@ -52,7 +52,7 @@ public class AREnitityEditor : Editor
 
         if (entity.copyRigidbods) CopyRigidBodies();
         if (entity.copyConfigJoints) CopyConfigurableJoints();
-        if (entity.copyColliders) ;// CopyColliders();
+        if (entity.copyColliders) CopyColliders() ;// CopyColliders();
         
     }
 
@@ -148,6 +148,10 @@ public class AREnitityEditor : Editor
         }
     }
 
+    /// <summary>
+    /// Tries to clone the configurable joint setup of the to-clone object + it's heirarchy. Though, settings, such
+    /// as connected body, are only copied if the "Copy Config Joint Settings" option is enabled
+    /// </summary>
     void CopyConfigurableJoints() {
         AREntity entity = (AREntity)target;
         if (entity.physicsCloneRoot == null) return;
@@ -173,8 +177,105 @@ public class AREnitityEditor : Editor
 
                 if (curJoint == null) curJoint = transToCopyTo.gameObject.AddComponent<ConfigurableJoint>();
                 if (curJoint == null) Debug.LogWarning("ARCopyARComponents failed to create ConfigurableJoint on " + copyFrom.name);
-                if(entity.copyConfigJointSettings) CopyConfigJointSettings(copyFrom, curJoint);
+                if(entity.copyConfigJointSettings) CopyConfigurableJointSettings(copyFrom, curJoint);
             }
+        }
+    }
+
+    /// <summary>
+    /// This function tries to mimic the connected body settings of the clone-source before 
+    /// going off to copy general settings via the extension method I implemented elsewhere
+    /// </summary>
+    /// <param name="toCopyFrom"></param>
+    /// <param name="toCopyTo"></param>
+    void CopyConfigurableJointSettings(ConfigurableJoint toCopyFrom, ConfigurableJoint toCopyTo) {
+        //first, gotta get the right connectedBody - hardest part
+        if (toCopyFrom.connectedBody != null) {
+            Transform getBodFrom = ((AREntity)target).transform.FindDeepChild(toCopyFrom.connectedBody.name);
+            if (getBodFrom == null)
+                Debug.LogWarning("Couldn't setup configurable joint " + toCopyTo.name + " connectedBody because could't find rigidbody on " + toCopyFrom.connectedBody.name);
+            else {
+                Rigidbody bod = getBodFrom.GetComponent<Rigidbody>();
+                toCopyTo.connectedBody = bod;
+            }
+        }
+        //now copy the rest of settings
+        toCopyTo.CopyMotionDataFrom(toCopyFrom);
+    }
+   
+    /// <summary>
+    /// 
+    /// </summary>
+    void CopyColliders() {
+        AREntity entity = (AREntity)target;
+        /**
+         collect all colliders.  
+         use to get names of objs in "CopyFrom" object that have colliders.
+         get one copy from obj.  Get it's colliders/types.
+        get equivalent obj in Copy to.  Get it's colliders/types.
+         if copyFrom obj has more colliders of that type, add some to the copyTo
+         then copy values from each copyFrom to the copyTo
+         put the copyFrom we just finished up into a list
+
+        pull the next collider from our list of all colliders. if it's gameobject is already in the finished list, skip and go to next
+        repeat until done.
+         **/
+
+        //Get all the colliders, so that we know which of the objs in the From heirarchy we need to worry about.
+        Collider[] allFromColliders = entity.physicsCloneRoot.GetComponentsInChildren<Collider>();
+        //Create list so we can remember which children in the heirarchy we've already copied colliders over for
+        List<Transform> alreadyCopied = new List<Transform>();
+
+        Transform curObj = null;
+        Transform copyTo = null;
+        //for each gameObject (NOT COLLIDER) represented in the allFromColliders list, we need to duplicate it's colliders
+        for (int i = 0; i < allFromColliders.Length; i++) {
+            curObj = allFromColliders[i].transform;
+            //if we've already handled the curObj in a previous iteration of the loop, we can skip it!
+            if (alreadyCopied.Contains(curObj)) continue;
+            //Find the equivalent child in the "to" heirarchy which we'll be copying to - if fail to find, warn and continue to next object
+            copyTo = entity.transform.FindDeepChild(curObj.name);
+            if (copyTo == null) {
+                Debug.LogWarning("Heirarchy mismatch error - Copy Colliders failed - child '" + curObj.name + "'  was not found in '" + this.name + "' which is being copied to.");
+                continue;
+            }
+            //alrighty, we need to get and copy all colliders of each type from "curObj" to "copyTo"
+            CopyIndividColliders<CapsuleCollider>(curObj, copyTo);
+            CopyIndividColliders<SphereCollider>(curObj, copyTo);
+            CopyIndividColliders<BoxCollider>(curObj, copyTo);
+            CopyIndividColliders<MeshCollider>(curObj, copyTo);
+
+            alreadyCopied.Add(curObj);
+        }
+        //Done-zo
+    }
+
+    void CopyIndividColliders<T>(Transform copyFrom, Transform copyTo) where T : Collider {
+        //Gather <T> colliders on "copyFrom"
+        T[] colsToCopy = copyFrom.GetComponents<T>();
+        //if none, return
+        if (colsToCopy == null || colsToCopy.Length == 0) return;
+        //Gather <T> colliders on "copyTo"
+        List<T> colsOnCopyTo = new List<T>(copyTo.GetComponents<T>());
+        //figure out the difference in numbers of <T> colliders between the 2
+        int difInNumOfCols = colsToCopy.Length - colsOnCopyTo.Count;
+        //Add colliders to "copyTo" until it has the same number as "copyFrom"
+        for (int i = 0; (i < difInNumOfCols); i++) {
+            colsOnCopyTo.Add(copyTo.gameObject.AddComponent<T>());
+        }
+
+        T curFrom = null;
+        T curTo = null;
+        //Now copyTo has at least as many <T> colliders as copyFrom - lets copy the data over
+        for (int i = 0; i < colsToCopy.Length; i++) {
+            curFrom = colsToCopy[i];
+            curTo = colsOnCopyTo[i];
+
+            //use correct data copy method... ye, it's a little dumb to have a generic function and then cast back to a specific class, but I can't be assed rn
+            if (typeof(T) == typeof(SphereCollider)) (curTo as SphereCollider).CopyDataFrom(curFrom as SphereCollider);
+            else if (typeof(T) == typeof(CapsuleCollider)) (curTo as CapsuleCollider).CopyDataFrom(curFrom as CapsuleCollider);
+            else if (typeof(T) == typeof(BoxCollider)) (curTo as BoxCollider).CopyDataFrom(curFrom as BoxCollider);
+            else if (typeof(T) == typeof(MeshCollider)) (curTo as MeshCollider).CopyDataFrom(curFrom as MeshCollider);
         }
     }
 
